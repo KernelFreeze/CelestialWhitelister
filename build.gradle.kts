1331c91b65b62fd2adddd2baba54b239d353bcd3
KernelFreeze <KernelFreeze@users.noreply.github.com>
2026-02-14 19:56:35 -0500

    chore: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "dev.celestelove"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 val paperLibrary by configurations.creating
 configurations.compileOnly.get().extendsFrom(paperLibrary)
